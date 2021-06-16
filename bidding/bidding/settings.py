65fc710fb34a2877a24271cd620270933c4d4075
vyom <68771081+vyom-32@users.noreply.github.com>
2021-06-16 15:42:37 +0530

    Update settings.py

--- bidding/bidding/settings.py
+++ bidding/bidding/settings.py
@@ -128,8 +128,8 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL =False
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bidmafia007@gmail.com'  
-EMAIL_HOST_PASSWORD = "msodqaowigryvpvb"
+EMAIL_HOST_USER = '#@gmail.com'  
+EMAIL_HOST_PASSWORD = "#"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
